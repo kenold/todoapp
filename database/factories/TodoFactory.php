df50472b158e679fde1a0c01fbbf5e0cfe2f0513
Kenold Beauplan <bkenold@gmail.com>
2018-04-16 10:00:33 -0400

    update faker params

--- database/factories/TodoFactory.php
+++ database/factories/TodoFactory.php
@@ -4,6 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Todo::class, function (Faker $faker) {
     return [
-        'name' => $faker->sentence($nbWords = 6, $variableNbWords = true)
+        'name' => $faker->sentence(6)
     ];
 });
