31305c4f427fdc2b31df59f21f63d95988ac167b
Kenold Beauplan <bkenold@gmail.com>
2018-04-16 15:37:29 -0400

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,25 @@
 */
 
 Route::get('/', 'TodoController@index');
-Route::post('/create/todo', 'TodoController@store');
-Route::get('/delete/todo/{id}', [
+
+Route::post('/todo/create', 'TodoController@store');
+
+Route::get('/todo/edit/{id}', [
+    'uses' => 'TodoController@edit',
+    'as' => 'todo.edit'
+]);
+
+Route::post('/todo/update/{id}', [
+    'uses' => 'TodoController@update',
+    'as' => 'todo.update'
+]);
+
+Route::get('/todo/delete/{id}', [
     'uses' => 'TodoController@destroy',
     'as' => 'todo.delete'
+]);
+
+Route::get('/todo/completed/{id}', [
+    'uses' => 'TodoController@completed',
+    'as' => 'todo.completed'
 ]);
