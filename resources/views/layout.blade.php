36d2462cda2da7b85b0f73a8bcd033a82996941c
Kenold Beauplan <bkenold@gmail.com>
2018-04-16 10:47:25 -0400

    update styles

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,10 +6,20 @@
     <title>ToDo App 1.0</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.0/css/bulma.min.css">
     <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+    <style>
+      a { color: inherit; }
+      hr { margin: 0.6rem 0; }
+      .todo-form { margin-bottom: 2rem; }
+    </style>
   </head>
   <body>
   <section class="section">
     <div class="container">
+      <h1 class="title">ToDo App 1.0</h1>
+      <p class="subtitle">
+        Create with <span class="has-text-danger">Laravel PHP</span> and <span class="has-text-primary">Bulma</span>!
+      </p>
+      <hr>
       @yield('content')
     </div>
   </section>
