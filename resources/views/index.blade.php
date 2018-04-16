c658d708cf11109b6adb104ace20c95c9f6953a0
Kenold Beauplan <bkenold@gmail.com>
2018-04-16 10:06:59 -0400

    add new CSS classes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,11 @@
 @section('content')
     <h1 class="title">ToDo App 1.0</h1>
       <p class="subtitle">
-        Create with <strong>Laravel PHP</strong> and <strong>Bulma</strong>!
+        Create with <span class="has-text-danger">Laravel PHP</span> and <span class="has-text-primary">Bulma</span>!
       </p>
+
+      <ul>
+          
+          <li>Todo Name</li>
+      </ul>
 @endsection
