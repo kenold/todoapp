cce528a13d04559479441ea8fb813b2fe55962fe
Kenold Beauplan <bkenold@gmail.com>
2018-04-16 15:37:10 -0400

    add if conditions

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,32 +1,32 @@
 @extends('layout')
 
 @section('content')
-    <div class="columns">
-        <div class="column is-three-fifths">
-            <div class="todo-form">
-                <form action="/create/todo" method="POST">
-                    {{ csrf_field() }}
-                    <input type="text" class="input is-large" name="name" placeholder="Create a new todo and press ENTER...">
-                </form>                
-            </div>                        
-        
-            <ul>
-                @foreach($todos as $todo)
-                    <li>
-                        <span class="icon has-text-info">
-                            <a href=""><i class="fas fa-edit"></i></a>
-                        </span>
-                        <span class="icon has-text-danger">
-                            <a href="{{ route('todo.delete', ['id' => $todo->id]) }}"><i class="fas fa-trash-alt"></i></a>
-                        </span>
-                        <span class="icon has-text-primary">
-                            <a href=""><i class="fas fa-check-circle"></i></a>
-                        </span>
-                        {{ $todo->name }}
-                        <hr>
-                    </li>
-                @endforeach
-            </ul>
-        </div>
-    </div>
+    <div class="todo-form">
+        <form action="todo/create" method="POST">
+            {{ csrf_field() }}
+            <input type="text" class="input is-large" name="name" placeholder="Create a new todo and press ENTER...">
+        </form>                
+    </div>                        
+
+    <ul>
+        @foreach($todos as $todo)
+            <li>
+                <span class="icon has-text-info">
+                    <a href="{{ route('todo.edit', ['id' => $todo->id]) }}"><i class="fas fa-edit"></i></a>
+                </span>
+                <span class="icon has-text-danger">
+                    <a href="{{ route('todo.delete', ['id' => $todo->id]) }}"><i class="fas fa-trash-alt"></i></a>
+                </span>
+                @if($todo->completed)
+                    <span class="icon has-text-primary">
+                @else
+                    <span class="icon has-text-grey-light">
+                @endif
+                    <a href="{{ route('todo.completed', ['id' => $todo->id]) }}"><i class="fas fa-check-circle"></i></a>
+                </span>
+                {{ $todo->name }}
+                <hr>
+            </li>
+        @endforeach
+    </ul>     
 @endsection
