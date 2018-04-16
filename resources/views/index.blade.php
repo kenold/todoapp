a143185e5687c1a8ba9edcabcb07797f1b18ff96
Kenold Beauplan <bkenold@gmail.com>
2018-04-16 10:00:14 -0400

    inital commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('content')
+    <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+@endsection
